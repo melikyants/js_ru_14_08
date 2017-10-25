351a3f28f61f15c6ccce1e524075413dcd3f7d4d
Natalia Melikyants <melikyants@gmail.com>
2017-10-25 14:04:11 +0200

    wip

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,7 +1,6 @@
 import { ADD_COMMENT, LOAD_COMMENTS, START, SUCCESS, LOAD_ARTICLE_COMMENT } from '../constants'
-import {normalizedComments} from '../fixtures'
 import {arrToMap} from './utils'
-import {Record} from 'immutable'
+import {OrderedMap, Map, Record} from 'immutable'
 
 const CommentRecord = Record({
   id: null,
@@ -10,7 +9,7 @@ const CommentRecord = Record({
 })
 
 const ReducerRecord = Record({
-  entities: arrToMap([],CommentRecord),
+  entities: new OrderedMap({}),
 })
 
 const defaultState = new ReducerRecord()
@@ -19,10 +18,11 @@ export default (state = defaultState, action) => {
 
     switch (type) {
         case ADD_COMMENT:
-            return {...state, [randomId]: {
-                ...payload.comment,
-                id: randomId
-            }}
+            // return {...state, [randomId]: {
+            //     ...payload.comment,
+            //     id: randomId
+            // }}
+            return state.setIn(['entities', randomId], new CommentRecord({...payload.comment, id: randomId}))
         
         case LOAD_ARTICLE_COMMENT + SUCCESS:
             return state.mergeIn(['entities'], arrToMap(response, CommentRecord))
